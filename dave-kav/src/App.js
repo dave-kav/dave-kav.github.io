1ea721ffa27f4cec96152bfb25273e1b82bdabf9
Dave Kavanagh <dave.kavanagh@apata.io>
2023-12-23 14:45:02 +0000

    profiler

--- dave-kav/src/App.js
+++ dave-kav/src/App.js
@@ -3,14 +3,15 @@ import { faBook, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import IconLink from './IconLink'
 
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>
-          David Kavanagh
+        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'flex-start'}}>
+          <p>David Kavanagh</p> 
+          <img src="./profile.jpeg" alt="Profile" style={{width: '80px', height: '80px', borderRadius: '50%', marginLeft: '20px', marginTop: '35px'}}/>
+          </div>
         <hr style={{borderTop: "1px dashed #8c8b8b", width: "100%"}}/>
 
         </h2>
--- dave-kav/src/IconLink.js
+++ dave-kav/src/IconLink.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const IconLink = ({ url, icon, text, className }) => (
-  <a
-    className={className}
-    href={url}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <FontAwesomeIcon icon={icon}/> {text}
-  </a>
+  <span>
+    <FontAwesomeIcon icon={icon}/>
+    <a
+        className={className}
+        style={{marginLeft: '10px'}}
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {text}
+      </a>
+  </span>
 );
 
 export default IconLink;
