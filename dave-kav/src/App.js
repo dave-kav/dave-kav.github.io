5b93700e048084cfa1c46614e578821c49f2d316
Dave Kavanagh <dave.kavanagh@apata.io>
2023-12-23 12:01:07 +0000

    react skeleton

---
+++ dave-kav/src/App.js
@@ -0,0 +1,29 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="./CV_2023.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Resume
+        </a>
+        <a
+          className="App-link"
+          href="https://guthub.com/dave-kav"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
