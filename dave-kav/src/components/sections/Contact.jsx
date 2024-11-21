bf6688e8719ed5dfb9fdc0508b63783d2b79f365
Dave Kavanagh <dave@evervault.com>
2024-11-21 16:25:28 +0000

    remove hover

--- dave-kav/src/components/sections/Contact.jsx
+++ dave-kav/src/components/sections/Contact.jsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { Container } from '../ui/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { useInView } from '../../hooks/useInView';
 import './Contact.css';
 
@@ -12,7 +12,6 @@ export const Contact = () => {
 
   const handleResumeClick = (e) => {
     e.preventDefault();
-    // Assuming resume.pdf is in the public folder
     const link = document.createElement('a');
     link.href = 'resume.pdf';
     link.download = 'david-kavanagh-resume.pdf';
@@ -31,16 +30,19 @@ export const Contact = () => {
         <div className="contact__content">
           <h2 className="contact__title">Get In Touch</h2>
           <p className="contact__description">
-            I'm currently open to new opportunities. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+            Find me on the links below.
           </p>
           <div className="contact__links">
             <a href="mailto:hello@davekav.com" className="contact__link">
+              <FontAwesomeIcon icon={faEnvelope} />
               <span>Email</span>
             </a>
             <a href="https://github.com/dave-kav" className="contact__link" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} />
               <span>GitHub</span>
             </a>
             <a href="https://linkedin.com/in/dave-kav" className="contact__link" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} />
               <span>LinkedIn</span>
             </a>
             <button 
