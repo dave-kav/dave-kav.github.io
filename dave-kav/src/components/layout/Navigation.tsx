4f5018747cddc086362d73a23e99e336e1e1b0c5
Dave Kavanagh <dave@evervault.com>
2024-12-01 13:57:22 +0000

    fix mobile blogs nav

--- dave-kav/src/components/layout/Navigation.tsx
+++ dave-kav/src/components/layout/Navigation.tsx
@@ -8,10 +8,15 @@ import './Navigation.css';
 
 export const Navigation: FC = () => {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
   const { theme, toggleTheme } = useTheme();
   const location = useLocation();
   const isHomePage = location.pathname === '/';
   const isBlogsPage = location.pathname === '/blogs';
+
+  useEffect(() => {
+    setIsMobileMenuOpen(false);
+  }, [location]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -26,14 +31,19 @@ export const Navigation: FC = () => {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
+      setIsMobileMenuOpen(false);
     }
+  };
+
+  const handleMobileNavigation = () => {
+    setIsMobileMenuOpen(false);
   };
 
   return (
     <nav className={`navigation ${isScrolled ? 'is-scrolled' : ''}`}>
       <Container>
         <div className="navigation__inner">
-          <Link to="/" className="navigation__logo">
+          <Link to="/" className="navigation__logo" onClick={handleMobileNavigation}>
             <img 
               src="/logo.svg" 
               alt="DK Logo" 
@@ -49,7 +59,7 @@ export const Navigation: FC = () => {
             )}
             {!isBlogsPage && (
               <Link to="/blogs" className="navigation__link">
-                Blogs
+                Blog
               </Link>
             )}
             {isHomePage && (
@@ -97,9 +107,59 @@ export const Navigation: FC = () => {
             >
               <FontAwesomeIcon icon={theme === 'dark' ? faSun : faMoon} />
             </button>
+            <button
+              className={`navigation__mobile-toggle ${isMobileMenuOpen ? 'is-active' : ''}`}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              aria-label="Toggle mobile menu"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
           </div>
         </div>
       </Container>
+
+      <div className={`navigation__mobile ${isMobileMenuOpen ? 'is-open' : ''}`}>
+        {!isHomePage && (
+          <Link to="/" className="navigation__mobile-link" onClick={handleMobileNavigation}>
+            Home
+          </Link>
+        )}
+        {!isBlogsPage && (
+          <Link to="/blogs" className="navigation__mobile-link" onClick={handleMobileNavigation}>
+            Blog
+          </Link>
+        )}
+        {isHomePage && (
+          <>
+            <a 
+              onClick={() => scrollToSection('experience')} 
+              className="navigation__mobile-link"
+            >
+              Experience
+            </a>
+            <a 
+              onClick={() => scrollToSection('education')} 
+              className="navigation__mobile-link"
+            >
+              Education
+            </a>
+            <a 
+              onClick={() => scrollToSection('projects')} 
+              className="navigation__mobile-link"
+            >
+              Projects
+            </a>
+            <a 
+              onClick={() => scrollToSection('contact')} 
+              className="navigation__mobile-link"
+            >
+              Contact
+            </a>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
