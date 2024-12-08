2c6414ab659d0d5caad88f80b48041e282af65c7
Dave Kavanagh <dave@evervault.com>
2024-12-08 10:41:17 +0000

    Add substack social link (#14)

--- dave-kav/src/utils/constants.ts
+++ dave-kav/src/utils/constants.ts
@@ -1,11 +1,13 @@
 interface SocialLinks {
   github: string;
   linkedin: string;
+  substack: string;
   email: string;
 }
 
 export const SOCIAL_LINKS: SocialLinks = {
   github: 'https://github.com/dave-kav',
   linkedin: 'https://linkedin.com/in/dave-kav',
+  substack: 'https://davekav.substack.com',
   email: 'work@dave-kav.com'
 } as const;
